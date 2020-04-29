bb1fcda507b15cbf1b3a08230be1090ebfb6fb13
calinzabalt <calinzabalt@gmail.com>
2020-04-29 12:11:45 +0300

    nuxt snipcart

---
+++ nuxt.config.js
@@ -0,0 +1,82 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css' }
+    ],
+    script: [
+      { src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js' },
+      { src: 'https://cdn.snipcart.com/scripts/2.0/snipcart.js', id: 'snipcart', 'data-api-key': 'YjZlMDRjZTUtNjE5YS00NWIxLTk5ZWUtMjIyNjc2MzEwMzU2NjM3MjE5MzQyMjcxMjE1Nzgz' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  devModules: [
+    '@nuxtjs/vuetify',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/pwa',
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/inspire.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout>
+   <button
+      className="snipcart-add-item"
+      data-item-id="capsuni"
+      data-item-price="79.99"
+      data-item-url="https://laughing-euclid-20b960.netlify.com/#/"
+      data-item-description="Capsuni Bio cultivati in natura afara fara conservanti sau coloranti"
+      data-item-image="../images/strawberry-2960533_640.jpg"
+      data-item-name="Capsuni!!!"
+    >
+      Add to cart
+    </button>
+  </v-layout>
+</template>
