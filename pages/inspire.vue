620c6ab3388b713f6888d1d1acfff0fbe01cc31b
Mastercalin <calinzabalt@gmail.com>
2020-04-29 12:26:53 +0300

    Update inspire.vue

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -4,7 +4,7 @@
       className="snipcart-add-item"
       data-item-id="capsuni"
       data-item-price="79.99"
-      data-item-url="https://laughing-euclid-20b960.netlify.com/#/"
+      data-item-url="https://priceless-johnson-fc16c9.netlify.app/inspire"
       data-item-description="Capsuni Bio cultivati in natura afara fara conservanti sau coloranti"
       data-item-image="../images/strawberry-2960533_640.jpg"
       data-item-name="Capsuni!!!"
